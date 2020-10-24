9261c3807619bf63d8b7dd3fea60bc0f33ede2e2
Bing Z <bing@bings-mbp.lan>
2020-10-24 16:49:40 -0600

    initial file folders setup, changed eslint indent error rule from 'tab' to 4

---
+++ config/config.js
@@ -0,0 +1,27 @@
+//Import doteve package 
+require("dotenv").config();
+
+//Export the config as an object
+module.exports = {
+    development: {
+        username: "root",
+        password: process.env.MYSQL_PASSWORD,
+        database: "furlodge_db",
+        host: "127.0.0.1",
+        dialect: "mysql"
+    }, 
+    development: {
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB_NAME,
+        host: process.env.MYSQL_HOST,
+        dialect: "mysql"
+    }, 
+    development: {
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB_NAME,
+        host: process.env.MYSQL_HOST,
+        dialect: "mysql"
+    }
+};
