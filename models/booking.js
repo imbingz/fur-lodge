29f48a706e78d27eca1bd32909bffd4d3e4ae818
Bing Z <bing@bings-mbp.lan>
2020-10-28 16:25:49 -0600

    added booking table and set association btwn booking n host

---
+++ models/booking.js
@@ -0,0 +1,41 @@
+//Create Booking Model and export the module
+
+module.exports = (sequelize, DataTypes) => {
+    //Create Booking Model
+    const Booking = sequelize.define("Booking", {
+        // eslint-disable-next-line camelcase
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // eslint-disable-next-line camelcase
+        lasst_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    });
+
+    Booking.associate = function(models) {
+        Booking.belongsTo(models.Host, {
+            foreignKey: {
+                name: "host_id",
+                allowNull: false,
+            },
+        });
+    };
+
+    //Return Booking Model
+    return Booking;
+};
