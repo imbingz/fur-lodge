5648b3a24a7ee60d672ae275d8b0d64dd698f9fe
Bing Z <bing@bings-mbp.lan>
2020-10-28 13:15:11 -0600

    edited passport, added initial Host table

--- config/passport.js
+++ config/passport.js
@@ -1,46 +1,46 @@
 // Import packages and modules 
 
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const db = require("../models/host");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../models");
 
-//Set up passport Local Stragey (use pasport LocalStrategy) and login with user's email and password 
+//Set up passport Local Stragey (use pasport LocalStrategy) and login with host's email and password 
 
 passport.use(new LocalStrategy(
-    //user sign in with an email rather than with a username
+    //host sign in with an email rather than with a username
     {
         usernameField: "email"
     },
     ((email, password, done) => {
-        //when a user click LOGIN button, this code runs
-        db.User.findOne({
+        //when a host click LOGIN button, this code runs
+        db.Host.findOne({
             where: {
                 email
             }
-        }).then((dbUser) => {
-            //If there is no user in the database with given email
-            if (!dbUser) {
+        }).then((dbHost) => {
+            //If there is no host in the database with given email
+            if (!dbHost) {
                 return done(null, false, {message: "Incorrect email."});
             }
             //if there is matching email but no matching password in the database
-            if (!dbUser.validPassword(password)) {
+            if (!dbHost.validPassword(password)) {
                 return done(null, false, {message: "Incorrect password."});
             } 
-            //If find matching email and password, return the user to the route handler
-            return done(null, dbUser);
+            //If find matching email and password, return the host to the route handler
+            return done(null, dbHost);
         });
 
     })
 ));
 
-//Sequelize serialize and deserialize user in order to help keep authentication state across HTTP requests 
+//Sequelize serialize and deserialize host in order to help keep authentication state across HTTP requests 
 //Boiler plate needed to make it all work 
-passport.serializeUser((user, cb) => {
-    cb(null, user);
+passport.serializeUser((host, cb) => {
+    cb(null, host);
 });
 
-passport.deserializeUser((user, cb) => {
-    cb(null, user);
+passport.deserializeUser((host, cb) => {
+    cb(null, host);
 });
 
 //Export the configured passport 
--- models/host.js
+++ models/host.js
@@ -1,37 +1,42 @@
-'use strict';
+//Import bcrypt package for password hashing (using bcryptjs version may cause errors on Windows machine sometimes)
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
+const bcrypt = require("bcryptjs");
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//Create Host Model and export the module
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = (sequelize, DataTypes) => {
+    //Create a Host model 
+    const Host = sequelize.define("host", {
+        //Set email attribute. check for valid email format and email cannot be null
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        //Password cannot be null 
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+  
+    //Create a custom method for Host Model. 
+    // Check if the unhashed password entered by the host matches any hashed password stored in the database 
+    Host.prototype.validPassword = function(password) {
+        //bcrypt.compareSync() returns true or false 
+        return bcrypt.compareSync(password, this.password);
+    };
+    
+    //Hooks (also known as callbacks or lifecycle events), are functions which are called before and after calls in sequelize are executed. They run during varoiud phases of the Host Model lifecycle
+    //Here before a host is created, hooks will automatically hash hosts password
+    Host.addHook("beforeCreate", host => {
+        host.password = bcrypt.hashSync(host.password, bcrypt.genSaltSync(10), null);
+    });
+    
+    //after hashing password, return Host Model
+    return Host;
+  
+};
