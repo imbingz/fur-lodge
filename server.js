2361665dba9746dd49ccb72d85bf43e9be680c20
Bing Z <bing@bings-mbp.lan>
2020-10-29 14:55:26 -0600

    setup handlebars, html-routes, isAuthenticated middelware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 const passport = require("./config/passport");
+const exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3000;
@@ -18,8 +19,12 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+//set handlebars - extname: '.hbs' inside exphbs
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 // Requiring routes
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
