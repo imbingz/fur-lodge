6b647be93b02651129ecc470a712c0894bb3915f
Vivid Green <zacharycreek@gmail.com>
2020-10-29 18:35:47 -0600

    Added more fields to booking.js.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,10 @@ const db = require("../models");
 
 module.exports = function (app) {
 
-    app.post("/host", async (req, res) => {
-        console.log(req.body);
-        const results = await db.Host.create(req.body);
-        try {
-            res.json(results);
-        } catch (error) {
-            console.log(error);
-        }
+    app.post("/host", (req, res) => {
+        db.Host.create(req.body)
+            .then(results => res.json(results))
+            .catch(error => console.log(error));
     });
 
     app.post("/booking", (req,res) => {
