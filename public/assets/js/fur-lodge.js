6c1ee04c952d58308b358c41217632cecaecebd8
cboswel1 <christianboswell86@gmail.com>
2020-10-31 15:28:02 -0600

    button working on index

--- public/assets/js/fur-lodge.js
+++ public/assets/js/fur-lodge.js
@@ -5,47 +5,33 @@
 /* eslint-disable no-unused-vars */
 //Wait for the document to load 
 
+$(() => { 
 
+    const searchForm = $("form#search");
+    const cityInput = $("input#city-input");
 
-$(document).ready(() => { 
-
-    $(document).on("submit", "#host-form", handleHostFormSubmit);
-    //function to submit search from info 
-
-    function handleHostFormSubmit(event) {
+    searchForm.on("submit", (event) => {
         event.preventDefault();
+        console.log("working");
+        
+        console.log($("#dogCare").val());
 
-        // if (city &&  &&) {
-        //     //Get hostData obj
-        //         const hostData = {
-        //             city: $("#city").val().trim(),
-        //             is_pup: isDog ? isDog : 0,
-        //             is_cat: isCat ? isCat : 0,
-        //             small: isSmall ? isSmall : 0,
-        //             med: isMedium ? isMedium : 0,
-        //             large: isLarge ? isLarge : 0,
-        //             giant: isGiant ? isGiant : 0,
-        //         };
-    
-        //         const { city, bio, is_pup, is_cat, short_term, long_term, pet_amt, small, med, large, giant } = hostData;
-    
-        //         //Call searchHost function 
-        //         searchHost( city, bio, is_pup, is_cat, short_term, long_term, pet_amt, small, med, large, giant );
-    
-        //         //Empty input fields
-        //         $("#city").val("");
-        //         $("#dog").prop("checked", false);
-        //         $("#cat").prop("checked", false);
-        //         $("#short-term").prop("checked", false);
-        //         $("#long-term").prop("checked", false);
-        //         $("#small").prop("checked", false);
-        //         $("#medium").prop("checked", false);
-        //         $("#large").prop("checked", false);
-        //         $("#giant").prop("checked", false);
-        //     } else {
-        //         alert("Rate and Pet Amount cannot be empty.");
-        //     }
-        // });
-    }
+        const userData = {
+            city: cityInput.val().trim(),
+            is_pup: isDog ? isDog : 0, 
+            is_cat: isCat ? isCat : 0,
+            short_term: isShortTerm ? isShortTerm : 0,
+            long_term: isLongTerm ? isLongTerm : 0,
+            pet_amt: petAmount,
+            small: isSmall ? isSmall : 0,
+            med: isMedium ? isMedium : 0,
+            large: isLarge ? isLarge : 0,
+            giant: isGiant ? isGiant : 0
+        }; 
+
+        //     if (!userData.city || !userData.)
+
+    });
 });
+
 
