d75eaedacc518415e2de7b7cdd972aeabae1c3a4
Bing Z <bing@bings-mbp.lan>
2020-10-31 16:46:05 -0600

    seach page works

--- public/assets/js/result.js
+++ public/assets/js/result.js
@@ -1,16 +1,16 @@
-$(()=> {
+// $(()=> {
 
-    const formData = {
-        "small": false,
-        "med": false,
-        "large": false,
-        "giant": false,
-        "is_pup": false,
-        "pet_amt": 1,
-        "available": true
-    };
+//     const formData = {
+//         "small": false,
+//         "med": false,
+//         "large": false,
+//         "giant": false,
+//         "is_pup": false,
+//         "pet_amt": 1,
+//         "available": true
+//     };
 
-    $.post("/result",formData)
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
-});
+//     $.post("/api/result",formData)
+//         .then((res) => console.log(res))
+//         .catch((err) => console.log(err));
+// });
