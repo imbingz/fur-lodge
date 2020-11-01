43d08250680619aea1e49aae98e67d9539e26b22
Bing Z <bing@bings-mbp.lan>
2020-11-01 14:35:35 -0700

    added comments n made the submit button click work

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -4,33 +4,13 @@
 
 $(() => {
 
-    // When the editProfile Button is clicked, we validate 
-    $("#editProfile-btn").on("click", function (event) {
+    // When the change submit Button is clicked, send changes to PUT request.
+    $("#submit-btn").click(dosomething); 
+    function dosomething(event) {
         event.preventDefault();
-        console.log("button clicked");
-      
-        const hostData = $(this).data("host");
-        console.log(hostData);
-      
+        console.log("submit button");
+        console.log(($(this).data("host")));
+        
+    }
 
-        // If we have an email and password we run the loginUser function and clear the form
-        // loginUser(userData.email, userData.password);
-        // emailInput.val("");
-        // passwordInput.val("");
-    });
-
-    // // loginUser does a post to our "api/login" route and if successful, redirects us the the profile page
-    // function loginUser(email, password) {
-    //     $.post("/api/login", {
-    //         email: email,
-    //         password: password,
-    //     })
-    //         .then(() => {
-    //             window.location.href = "/profile";
-    //             // If there's an error, log the error
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
-    // }
 });
