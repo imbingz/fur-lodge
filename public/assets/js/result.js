12be4a5ce64a6cb862d0c5784bb0047dd35b4dc8
Bing Z <bing@bings-mbp.lan>
2020-11-01 23:52:55 -0700

    get seeker info

--- public/assets/js/result.js
+++ public/assets/js/result.js
@@ -1,8 +1,25 @@
+/* eslint-disable camelcase */
 //Wait for page to finish loading
 $(() => {
-    $("form.booking").on("submit", $("#submitBooking-btn"), (e) => {
-        e.preventDefault();
-        console.log("Hi");
-    });
+    //Add event listener 
+    $("form.booking").on("submit", $("#submitBooking-btn"), getSeekerInfo);
+  
+    //Get seeker info 
+    function getSeekerInfo(event) {
+        event.preventDefault();
+        console.log("booking submit");
+        const seekerInfo = {
+            first_name: $("#first-name").val().trim(),
+            last_name: $("#last-name").val().trim(),
+            email: $("#email").val().trim(),
+            phone: $("#phone").val().trim(),
+        };
+      
+        sendSeekerInfo(seekerInfo);
+    }
 
+    function sendSeekerInfo(seekerInfo) {
+        console.log(seekerInfo);
+        //ajax call
+    }
 });
