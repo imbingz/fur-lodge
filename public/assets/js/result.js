699a40a81942749f3dc5648d5b945185dba3c43b
Bing Z <bing@bings-mbp.lan>
2020-11-01 23:01:22 -0700

    made button event listener work

--- public/assets/js/result.js
+++ public/assets/js/result.js
@@ -1,16 +1,8 @@
-// $(()=> {
+//Wait for page to finish loading
+$(() => {
+    $("form.booking").on("submit", $("#submitBooking-btn"), (e) => {
+        e.preventDefault();
+        console.log("Hi");
+    });
 
-//     const formData = {
-//         "small": false,
-//         "med": false,
-//         "large": false,
-//         "giant": false,
-//         "is_pup": false,
-//         "pet_amt": 1,
-//         "available": true
-//     };
-
-//     $.post("/api/result",formData)
-//         .then((res) => console.log(res))
-//         .catch((err) => console.log(err));
-// });
+});
